a9614974cc93cd6a7a1dddabdc60d605d33fed5a
bronze1man <brozne1man@gmail.com>
2012-07-19 19:31:39 +0800

    123

--- Foundation/caches/caches_template/default/content/footer.php
+++
@@ -1,6 +0,0 @@
-<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?>    <!-- footer -->
-    <div id="footer">
-    </div>
-</body>
-<script src="/statics/foundation/core/sea.js" data-main="init" type="text/javascript"> </script>
-</html>
--- Foundation/caches/caches_template/default/content/index.php
+++
@@ -1,8 +0,0 @@
-<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><?php include template("content","header"); ?>
-<!-- main -->
-    <div id="main">
-        <input type="checkbox" class="checkbox" />
-        <h1>asdfadfs</h1>
-    </div>
-<!-- END main -->
-<?php include template("content","footer"); ?>
