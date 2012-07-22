3783c8f427b347e2096307f25d9bb8fb694c53a5
施扬 <shiyang@epptime.com>
2012-07-22 10:46:10 +0800

    修改BootstrapCSS源码

--- core/init.js
+++ core/init.js
@@ -10,21 +10,21 @@ seajs.config({
 
     // 配置别称
     alias : {
-        'underscore' : '/path/to/lib/underscore-min.js',
-        'jquery' : '/path/to/lib/jquery-1.7.2.min.js',
-        'backbone' : '/path/to/lib/backbone-min.js',
-        'pngFix' : '/path/to/lib/DD_belatedPNG_0.0.8a-min.js'
+        'underscore' : '/statics/foundation/lib/underscore-min.js',
+        'jquery' : '/statics/foundation/lib/jquery-1.7.2.min.js',
+        'backbone' : '/statics/foundation/lib/backbone-min.js',
+        'pngFix' : '/statics/foundation/lib/DD_belatedPNG_0.0.8a-min.js'
     },
 
     // 预加载
     preload : [
         'plugin-text',
-        this.JSON ? '' : '/path/to/lib/json',
-        Function.prototype.bind ? '' : '/path/to/lib/es5-safe'
+        this.JSON ? '' : '/statics/foundation/lib/json',
+        Function.prototype.bind ? '' : '/statics/foundation/lib/es5-safe'
     ],
 
     charset : 'utf-8'
 });
 
 // 加载主文件
-seajs.use('/path/to/root');
+seajs.use('/statics/foundation/root');
