3e990ff42ef8fc98b4e6fb65f57f10126944a59b
Degas <smallsmallwolf@gmail.com>
2012-07-30 12:48:51 +0800

    增加modernizer，检测浏览器对HTML5以及CSS3支持
    放到init.js的preload中

--- core/init.js
+++ core/init.js
@@ -10,21 +10,22 @@ seajs.config({
 
     // 配置别称
     alias : {
-        'underscore' : 'path/lib/underscore-min.js',
-        'jquery' : 'path/lib/jquery-1.7.2.min.js',
-        'backbone' : 'path/lib/backbone-min.js',
-        'pngFix' : 'path/lib/DD_belatedPNG_0.0.8a-min.js'
+        'underscore' : './lib/underscore-min.js',
+        'jquery' : './lib/jquery-1.7.2.min.js',
+        'backbone' : './lib/backbone-min.js',
+        'pngFix' : './lib/DD_belatedPNG_0.0.8a-min.js'
     },
 
     // 预加载
     preload : [
+        './lib/modernizr.min',
         'plugin-text',
-        this.JSON ? '' : 'path/lib/json',
-        Function.prototype.bind ? '' : 'path/lib/es5-safe'
+        window.JSON ? '' : './lib/json',
+        Function.prototype.bind ? '' : './lib/es5-safe'
     ],
 
     charset : 'utf-8'
 });
 
 // 加载主文件
-seajs.use('path/root');
+seajs.use('./root');
